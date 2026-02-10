51a13bb92da4246c5b62259cbefcb9067907c987
seyi-gh <aesioseyuri@gmail.com>
2026-02-09 20:10:11 -0500

    fix: configure tailwind v4 correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 //! (1) Generate the page at landing in >Vercel< (2) [modular generation]
 
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless'; //! (1)
 
 export default defineConfig({
   output: 'server',
-  integrations: [react(), tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
